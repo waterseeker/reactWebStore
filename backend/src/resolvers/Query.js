edd77d53823cd293aac5e368463e94f79d7e32e3
Wayne Burris <waterseeker@hotmail.com>
2019-05-31 11:43:59 -0500

    added create mutation and query for items in the backend

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+// if your query is exactly the same on your Prisma and your Yoga,
+// you can just forward that query from Yoga to Prisma so you don't have to
+// duplicate the code for the query by using 'forwardto'
+
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db'),
+    // this code is replaced by the above use of forwardTo
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // },
+};
 
 module.exports = Query;
