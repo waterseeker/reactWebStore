1f45d12349b12eac438022979845f6416df7eaf3
Wayne Burris <waterseeker@hotmail.com>
2019-06-06 14:19:27 -0500

    adding users and permissions

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: Check if they're logged in
@@ -34,6 +37,32 @@ const Mutations = {
         //TODO
         // 3. Delete the item.
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase their password
+        args.email = args.email.toLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the database
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                    ...args,
+                    password,
+                    permissions: { set: ['USER'] },
+                },
+            },
+            info
+        );
+        // create the JSON Web Token(jwt) for the user
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+        // return the user to the browser
+        return user;
     },
 };
 
